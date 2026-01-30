6063ef669e9c0db1eed21bf48b5166f5ab50b0f6
spiicez21 <yuga.bharathijai2106@gmail.com>
2026-01-30 08:06:47 +0530

    feat: Scaffold new Android Compose project with Gradle wrapper, Firebase, and Hilt dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
-    id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("com.android.application") version "8.6.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.dagger.hilt.android") version "2.52" apply false
 }
