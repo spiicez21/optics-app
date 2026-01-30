8bcfb386d865b45d75b21cfefbbba2bf9736d275
spiicez21 <yuga.bharathijai2106@gmail.com>
2026-01-30 10:42:47 +0530

    feat: implement initial MainActivity with Compose UI and navigation graph.

--- app/src/main/java/com/opticalshop/presentation/MainActivity.kt
+++ app/src/main/java/com/opticalshop/presentation/MainActivity.kt
@@ -6,10 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.opticalshop.presentation.navigation.NavGraph
 import com.opticalshop.presentation.theme.OpticalShopTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,30 +18,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             OpticalShopTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Optical Shop")
+                    val navController = rememberNavController()
+                    NavHostControllerWrapper(navController)
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OpticalShopTheme {
-        Greeting("Android")
+    @androidx.compose.runtime.Composable
+    private fun NavHostControllerWrapper(navController: androidx.navigation.NavHostController) {
+        NavGraph(navController = navController)
     }
 }
